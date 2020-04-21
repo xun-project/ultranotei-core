09b23228d4c0587aee24951afb1895fbb94ceedb
UltraNote Support <support@ultranote.org>
2020-04-21 20:21:37 +0000

    Checkpoints.h correction

--- src/CryptoNoteCore/Checkpoints.h
+++ src/CryptoNoteCore/Checkpoints.h
@@ -5,10 +5,7 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#pragma once
-#include <map>
-#include "CryptoNoteBasicImpl.h"
-#include <Logging/LoggerRef.h>
+
 
 #pragma once
 #include <map>
