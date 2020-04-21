6ef174fbfca9bc0a89d5dec436609d5eea9c7ea5
UltraNote Support <support@ultranote.org>
2020-04-21 20:11:45 +0000

    Checkpoints.h remove DNS

--- src/CryptoNoteCore/Checkpoints.h
+++ src/CryptoNoteCore/Checkpoints.h
@@ -10,6 +10,11 @@
 #include "CryptoNoteBasicImpl.h"
 #include <Logging/LoggerRef.h>
 
+#pragma once
+#include <map>
+#include "CryptoNoteBasicImpl.h"
+#include <Logging/LoggerRef.h>
+
 namespace CryptoNote
 {
   class Checkpoints
@@ -19,10 +24,14 @@ namespace CryptoNote
 
     bool add_checkpoint(uint32_t height, const std::string& hash_str);
     bool is_in_checkpoint_zone(uint32_t height) const;
+    bool load_checkpoints_from_file(const std::string& fileName);
+    bool load_checkpoints_from_dns();
+    bool load_checkpoints();    
     bool check_block(uint32_t height, const Crypto::Hash& h) const;
     bool check_block(uint32_t height, const Crypto::Hash& h, bool& is_a_checkpoint) const;
     bool is_alternative_block_allowed(uint32_t blockchain_height, uint32_t block_height) const;
-
+    std::vector<uint32_t> getCheckpointHeights() const;
+    
   private:
     std::map<uint32_t, Crypto::Hash> m_points;
     Logging::LoggerRef logger;
