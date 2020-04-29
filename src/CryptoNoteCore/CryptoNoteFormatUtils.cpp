0c2141c1ec8e6d2caddbf24d3cae4e8e7d9bf52b
UltraNote Support <support@ultranote.org>
2020-04-29 22:51:05 +0000

    CryptoNoteFormatUtils.cpp remove old versions

--- src/CryptoNoteCore/CryptoNoteFormatUtils.cpp
+++ src/CryptoNoteCore/CryptoNoteFormatUtils.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2011-2017 The Cryptonote developers
 // Copyright (c) 2017-2018 The Circle Foundation & Conceal Devs
 // Copyright (c) 2018-2019 Conceal Network & Conceal Devs
+// Copyright (c) 2017-2020 UltraNote developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -488,9 +489,9 @@ bool get_block_longhash(cn_context &context, const Block& b, Hash& res) {
     return false;
   }
 
-if (b.majorVersion >= 7) {
+if (b.majorVersion >= 3) {
     cn_conceal_slow_hash_v0(context, bd.data(), bd.size(), res);
-  } else if (b.majorVersion >= 3) {
+  } else if (b.majorVersion == 2) {
     cn_fast_slow_hash_v1(context, bd.data(), bd.size(), res);
   } else {
     cn_slow_hash(context, bd.data(), bd.size(), res);
