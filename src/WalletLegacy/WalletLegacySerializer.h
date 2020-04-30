bd3adf869d2c2892d7de879e010aaf178d55714d
UltraNote Support <support@ultranote.org>
2020-04-30 06:10:38 +0000

    WalletLegacySerializer.h add chacha8

--- src/WalletLegacy/WalletLegacySerializer.h
+++ src/WalletLegacy/WalletLegacySerializer.h
@@ -1,6 +1,7 @@
 // Copyright (c) 2011-2017 The Cryptonote developers
 // Copyright (c) 2017-2018 The Circle Foundation & Conceal Devs
 // Copyright (c) 2018-2019 Conceal Network & Conceal Devs
+// Copyright (c) 2017-2020 UltraNote developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -11,7 +12,7 @@
 #include <istream>
 
 #include "crypto/hash.h"
-#include "crypto/chacha.h"
+#include "crypto/chacha8.h"
 
 namespace CryptoNote {
 class AccountBase;
@@ -34,8 +35,8 @@ private:
   void saveKeys(CryptoNote::ISerializer& serializer);
   void loadKeys(CryptoNote::ISerializer& serializer);
 
-  Crypto::chacha_iv encrypt(const std::string& plain, const std::string& password, std::string& cipher);
-  void decrypt(const std::string& cipher, std::string& plain, Crypto::chacha_iv iv, const std::string& password);
+  Crypto::chacha8_iv encrypt(const std::string& plain, const std::string& password, std::string& cipher);
+  void decrypt(const std::string& cipher, std::string& plain, Crypto::chacha8_iv iv, const std::string& password);
 
   CryptoNote::AccountBase& account;
   WalletUserTransactionsCache& transactionsCache;
