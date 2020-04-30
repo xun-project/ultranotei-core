677032cee77d20c3921470f14439400c76bba9f4
UltraNote Support <support@ultranote.org>
2020-04-30 01:26:38 +0000

    DaemonCommandsHandler.h edited enabled cores

--- src/Daemon/DaemonCommandsHandler.h
+++ src/Daemon/DaemonCommandsHandler.h
@@ -1,6 +1,7 @@
 // Copyright (c) 2011-2017 The Cryptonote developers
 // Copyright (c) 2017-2018 The Circle Foundation & Conceal Devs
 // Copyright (c) 2018-2019 Conceal Network & Conceal Devs
+// Copyright (c) 2017-2020 UltraNote developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -64,4 +65,5 @@ private:
 
   bool start_mining(const std::vector<std::string>& args);
   bool stop_mining(const std::vector<std::string>& args);
+  bool status(const std::vector<std::string>& args);
 };
