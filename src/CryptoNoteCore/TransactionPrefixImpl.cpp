1aa11cdac74e2c4e7d509caffa2d2e8021e06a0a
UltraNote Support <support@ultranote.org>
2020-12-20 01:36:24 +0000

    TransactionPrefixImpl.cpp memory

--- src/CryptoNoteCore/TransactionPrefixImpl.cpp
+++ src/CryptoNoteCore/TransactionPrefixImpl.cpp
@@ -1,12 +1,15 @@
 // Copyright (c) 2011-2017 The Cryptonote developers
 // Copyright (c) 2017-2018 The Circle Foundation & Conceal Devs
 // Copyright (c) 2018-2019 Conceal Network & Conceal Devs
+// Copyright (c) 2018-2020 UltraNote Network & UlraNote Devs
+
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "ITransaction.h"
 
 #include <numeric>
+#include <memory>
 #include <system_error>
 
 #include "CryptoNoteCore/CryptoNoteBasic.h"
