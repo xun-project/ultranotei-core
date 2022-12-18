3e1944e1c4a0f0d43c1d11ee00e910a3d23caca5
ultranote <support@ultranote.org>
2022-12-18 19:44:13 +0100

    enable-blockchain-indexes option

--- src/Daemon/Daemon.cpp
+++ src/Daemon/Daemon.cpp
@@ -126,6 +126,9 @@ int main(int argc, char* argv[])
 
     po::options_description desc_cmd_only("Command line options");
     po::options_description desc_cmd_sett("Command line options and settings options");
+	
+	   desc_cmd_sett.add_options() 
+      ("enable-blockchain-indexes,i", po::bool_switch()->default_value(false), "Enable blockchain indexes");
 
     command_line::add_arg(desc_cmd_only, command_line::arg_help);
     command_line::add_arg(desc_cmd_only, command_line::arg_version);
@@ -236,7 +239,8 @@ int main(int argc, char* argv[])
     }
 
     CryptoNote::Currency currency = currencyBuilder.currency();
-    CryptoNote::core ccore(currency, nullptr, logManager);
+    
+	CryptoNote::core ccore(currency, nullptr, logManager, vm["enable-blockchain-indexes"].as<bool>());
 
      CoreConfig coreConfig;
     coreConfig.init(vm);
